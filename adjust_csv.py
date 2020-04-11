08dd2200ca3d207018cfebc68c2a95af5d93462a
chandratop <chandratop.mail@gmail.com>
2020-04-11 19:56:42 +0530

    some adjustments

--- adjust_csv.py
+++ adjust_csv.py
@@ -1,17 +1,29 @@
-f = open('green_new.csv', 'r')
-new_f = open('green_above_75.csv', 'w')
+'''
+This code does the mapreduce on the existing heatmap created.
+We create a new csv file from the old green_new.csv and set all
+percentage values >= 75 as 100 and the others as 0.
+When we generate the heatmap again we get the places
+on the map with atleast 75% greenery
+'''
+f = open('csv_files\green_new.csv', 'r')
+new_f = open('csv_files\green_above_75.csv', 'w')
 for line in f:
+    # getting the positions of certain characters for extracting the percentage
     underscore = line.find('_')
     hyphen = line.find('-')
     dot = line.find('.')
     start = line.find(',')
     end = start + 1 + line[start + 1:].find(',')
+    # extracting the number
     number = line[start + 1:end]
     number = float(number)
+    # we need the x and y values to output in the csv again
     xval = line[underscore + 1:hyphen]
-    yval = line[hyphen+1:dot]
+    yval = line[hyphen + 1:dot]
+    # manual mapreduce
     if number >= 75:
         number = 100
+        # printing the box coordinates for later use to find actual coords
         s = str(xval)+','+str(yval)
         print(s)
     else:
--- create_heatmap.py
+++ create_heatmap.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("green_above_75.csv")
 # 202x 113
 result = df.pivot(index='yval', columns='xval', values='Percentage')
-# labels = (np.asarray(["{0} \n {1:.2f}".format(a, p)
-                    # for a, p in zip(Area.flatten(), Percentage.flatten())])).reshape(202, 113)
 
 fig, ax = plt.subplots(figsize=(12, 7))
 title = "Greenery"
