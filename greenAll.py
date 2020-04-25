00c2801bad78371ffe7ee007b02f9e44360e3964
VigneshKKumar <vigneshkkumar127@gmail.com>
2020-04-25 15:30:04 +0530

    Updated heatmaps

---
+++ greenAll.py
@@ -0,0 +1,11 @@
+og = open("csv_files/green400090255255.csv","r")
+new = open("csv_files/green_all.csv","w")
+print("Area,Percentage,xval,yval",file = new)
+
+for i in og.readlines():
+    Val = i.split(",")[0][4:-4]
+    Val = Val.replace('-',',')
+    print(i[:-1],Val,sep = ",",file = new)
+
+og.close()
+new.close()
